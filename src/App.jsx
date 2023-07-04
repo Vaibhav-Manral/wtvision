382b7dea9c4d2c307491cd7a91a0670def8a10d7
vab096 <vabmanral@gmail.com>
2023-07-04 21:48:17 +0530

    upload

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [move,setmove]=useState(2);
+  // const [color,setcolor]=useState(true);
+  const [index,setindex]=useState(0);
+
+  const tabs = ["Runs", "Noball", "LegBye", "Bye", "Wide"];
+
+ 
+
+const show=(el,i)=>{
+  if(i==0){
+    setmove(2)
+  }else{
+    setmove(i*20)
+  }
+    setindex(i)
+}
+
+  return (
+      <div className='maindiv'>
+        <div className='item' style={{left:`${move}%`}}>
+        </div>
+        {
+          tabs.map((el, i) => {
+            return (
+              <div className='_item1' style={{color:i==index?"#fff":"#808080"}} 
+              onClick={()=>show(el,i)}>
+              {el}
+              </div>  
+            )
+          })
+        }
+      </div>
+
+  )
+}
+
+export default App
